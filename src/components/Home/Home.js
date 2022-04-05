8fa9de2bab0bb5894e1da1eeff054741d267492f
nafish08 <md.nafish.sadik@gmail.com>
2022-04-05 21:17:10 +0600

    ReadMe file edited and Netlify Page not found fixed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,6 +19,7 @@ const Home = () => {
                             manufacturing process is going to profoundly change the way we
                             experience technology in the future. But of course they think
                             this way—it sometimes actually happens.</p>
+                        <button className='text-lg bg-orange-500 text-white font-semibold px-4 py-2 rounded'>Live Demo</button>
                     </div>
                 </div>
                 <div className='flex justify-center items-center'>
