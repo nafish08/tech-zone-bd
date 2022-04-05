4a4f85487f16ac9b4953fe48557b236b910b6a62
nafish08 <md.nafish.sadik@gmail.com>
2022-04-05 19:16:52 +0600

    404 Not Found page route added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div>
+            <img className='w-96 mx-auto' src="image/404.jpg" alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
