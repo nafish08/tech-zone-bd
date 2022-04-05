b42bd4eebbd4f620104fa72623ee6b1390cdb4e5
nafish08 <md.nafish.sadik@gmail.com>
2022-04-05 16:56:45 +0600

    Components created & Routes added with navbar links

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <div>
+                    <Link to="/home">Home</Link>
+                    <Link to="/reviews">Reviews</Link>
+                    <Link to="/dashboard">Dashboard</Link>
+                    <Link to="/blogs">Blogs</Link>
+                    <Link to="/about">About</Link>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
