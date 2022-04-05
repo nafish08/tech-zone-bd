a3d2de0697f8424ec7059472bf9ec5307742cacc
nafish08 <md.nafish.sadik@gmail.com>
2022-04-05 19:30:03 +0600

    Custom link added with the navigation menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 import './Header.css';
 
 const Header = () => {
     return (
         <div>
-            <nav className='nav-bar'>
-                <div className='text-3xl uppercase p-4'>
-                    <Link to="/home">Home</Link>
-                    <Link to="/reviews">Reviews</Link>
-                    <Link to="/dashboard">Dashboard</Link>
-                    <Link to="/blogs">Blogs</Link>
-                    <Link to="/about">About</Link>
+            <nav className='nav-bar flex justify-center'>
+                <div className='text-3xl uppercase p-4 flex'>
+                    <CustomLink to="/home">Home</CustomLink>
+                    <CustomLink to="/reviews">Reviews</CustomLink>
+                    <CustomLink to="/dashboard">Dashboard</CustomLink>
+                    <CustomLink to="/blogs">Blogs</CustomLink>
+                    <CustomLink to="/about">About</CustomLink>
                 </div>
             </nav>
         </div>
